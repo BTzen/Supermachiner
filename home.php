69fb2a09f69403d368912f4435816c28ef6f00c7
BTzen <matthew_fb@live.ca>
2016-03-11 15:35:41 -0500

    minor style and content changes

--- home.php
+++ home.php
@@ -1,31 +1,45 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<!-- <link rel="stylesheet" type="text/css" href="reset.css" /> -->
+	<link rel="stylesheet" type="text/css" href="styles.css" />
 	<title>One Paw Media</title>
 </head>
 <body>
-	<h1>This is a test</h1>
-	<p>lorem ipsum</p>
-	
-	<?php
-		require_once("config.php");
-		//set up connection to db
-		try {
-			$pdo = new PDO(DSN, DBUSER, DBPASS);
-			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	<div id="wrapper">
+		<nav>
+			<ul>
+				<li><a href="#">Playlists</a></li>
+				<li><a href="#">Upload</a></li>
+				<li><a href="#">Settings</a></li>
+			</ul>
+		</nav>
+		
+		<div id="main">
+			<h1>This is a test</h1>
+			<p>lorem ipsum</p>
 			
-			$sql = "select * from files";
-			$result = $pdo->query($sql);
-			
-			while ($row = $result->fetch()) {
-				echo $row['id'] . $row['title'] . "<br />";
-			}
-			
-			$pdo = null;							//close connection
-		}
-		catch (PDOException $e) {
-			die( $e->getMessage() );
-		}
-	?>
+			<?php
+				require_once("config.php");
+				//set up connection to db
+				try {
+					$pdo = new PDO(DSN, DBUSER, DBPASS);
+					$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+					
+					$sql = "select * from files";
+					$result = $pdo->query($sql);
+					
+					while ($row = $result->fetch()) {
+						echo $row['id'] . $row['title'] . "<br />";
+					}
+					
+					$pdo = null;							//close connection
+				}
+				catch (PDOException $e) {
+					die( $e->getMessage() );
+				}
+			?>
+		</div>
+	</div>
 </body>
 </html>
